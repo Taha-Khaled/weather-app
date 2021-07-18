0cfec2c2e42646af2db131be663ed8aee5893228
taha apps <appsbytaha@gmail.com>
2021-07-19 00:51:46 +0200

    Updates

---
+++ precache-manifest.0196253799650a4ac8ed3b7d2d900310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5125a55331a6d769365eb8ec6a540702",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "847a6b7194edbe4c9abc",
+    "url": "/weather-app/static/css/main.945e485b.chunk.css"
+  },
+  {
+    "revision": "e7b59ac894d8f161a080",
+    "url": "/weather-app/static/js/2.47204bc6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-app/static/js/2.47204bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847a6b7194edbe4c9abc",
+    "url": "/weather-app/static/js/main.80fa992e.chunk.js"
+  },
+  {
+    "revision": "786ac934567c6f40ebab",
+    "url": "/weather-app/static/js/runtime-main.e935355a.js"
+  }
+]);
